02c842e63be4acb55ac110a8788e5f009151ec52
osi <yaoiluo@gmail.com>
2019-06-02 20:30:17 +0800

    :green_heart: psr2

--- src/Commands/GroupAdd.php
+++ src/Commands/GroupAdd.php
@@ -8,39 +8,41 @@ use Lauthz\Facades\Enforcer;
 /**
  * PolicyAdd class.
  */
-class GroupAdd extends Command {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'group:add
+class GroupAdd extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'group:add
                             {policy : the rule separated by commas}';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Adds a role inheritance rule to the current policy.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds a role inheritance rule to the current policy.';
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle() {
-		$params = explode(',', $this->argument('policy'));
-		array_walk($params, function (&$value) {
-			$value = trim($value);
-		});
-		$ret = Enforcer::addGroupingPolicy(...$params);
-		if ($ret) {
-			$this->info('Grouping `' . implode(', ', $params) . '` created');
-		} else {
-			$this->error('Grouping `' . implode(', ', $params) . '` creation failed');
-		}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $params = explode(',', $this->argument('policy'));
+        array_walk($params, function (&$value) {
+            $value = trim($value);
+        });
+        $ret = Enforcer::addGroupingPolicy(...$params);
+        if ($ret) {
+            $this->info('Grouping `' . implode(', ', $params) . '` created');
+        } else {
+            $this->error('Grouping `' . implode(', ', $params) . '` creation failed');
+        }
 
-		return $ret ? 0 : 1;
-	}
+        return $ret ? 0 : 1;
+    }
 }
