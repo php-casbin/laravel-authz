6a4ff69e82b66b1eb512da49af282bcd48cb8b0d
Jon Lee <techlee@qq.com>
2022-01-28 12:09:35 +0800

    feat: compatible with lumen

--- src/LauthzServiceProvider.php
+++ src/LauthzServiceProvider.php
@@ -15,8 +15,10 @@ class LauthzServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([__DIR__ . '/../database/migrations' => database_path('migrations')], 'laravel-lauthz-migrations');
-            $this->publishes([__DIR__ . '/../config/lauthz-rbac-model.conf' => config_path('lauthz-rbac-model.conf')], 'laravel-lauthz-config');
-            $this->publishes([__DIR__ . '/../config/lauthz.php' => config_path('lauthz.php')], 'laravel-lauthz-config');
+            $this->publishes([
+                __DIR__ . '/../config/lauthz-rbac-model.conf' => $this->app->basePath() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . ('lauthz-rbac-model.conf'),
+                __DIR__ . '/../config/lauthz.php' => $this->app->basePath() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . ('lauthz.php'),
+            ], 'laravel-lauthz-config');
 
             $this->commands([
                 Commands\GroupAdd::class,
